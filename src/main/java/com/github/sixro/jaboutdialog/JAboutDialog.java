380f25ea60d0185acd646cdca259a051d5a19e5a
Sixro <me@sixro.net>
2018-06-23 01:55:33 +0200

    Now we show app content, system info and added a good example of how to use it

--- src/main/java/com/github/sixro/jaboutdialog/JAboutDialog.java
+++ src/main/java/com/github/sixro/jaboutdialog/JAboutDialog.java
@@ -3,18 +3,44 @@ package com.github.sixro.jaboutdialog;
 import java.awt.BorderLayout;
 import java.awt.GraphicsConfiguration;
 import java.awt.Window;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JEditorPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.text.Document;
+import javax.swing.text.html.HTMLEditorKit;
 
+/**
+ * Represents an <i>about</i> dialog used to describe your application, version, credits, system info.
+ * 
+ * <p>
+ * It contains two tab panes. The first one is called &quot;Application&quot; and contains a description of your application.<br/>
+ * In order to set the text use {@link #setApplicationContent(String)}. It accepts an HTML text that you
+ * can use to describe every aspect of your application.<br/>
+ * Take a look at the {@code test} folder of the library containing an example on how to use it.
+ * 
+ * The second tab contains some system info. E.g. it contains the list of all system properties
+ * available at the moment in the application.
+ * </p>
+ */
 public class JAboutDialog extends JDialog {
 
 	private static final int BORDER_WIDTH = Integer.getInteger("swing.default.border.width", 12);
 	
 	private static final long serialVersionUID = 1L;
+
+	private JEditorPane applicationContentPane;
 
 	public JAboutDialog(Window owner, ModalityType modalityType) {
 		super(owner, modalityType);
@@ -36,15 +62,51 @@ public class JAboutDialog extends JDialog {
 		createUI();
 	}
 
+	public void setApplicationContent(String html) {
+		applicationContentPane.setText(html);
+	}
+	
 	private void createUI() {
 		setLayout(new BorderLayout());
 		((JPanel) getContentPane()).setBorder(BorderFactory.createEmptyBorder(BORDER_WIDTH, BORDER_WIDTH, BORDER_WIDTH, BORDER_WIDTH));
 
+		// Application content
 		JTabbedPane tabbedPane = new JTabbedPane();
-		tabbedPane.add("Application", new JPanel());
-		tabbedPane.add("System", new JPanel());
+		applicationContentPane = new JEditorPane();
+		applicationContentPane.setEditable(false);
+		HTMLEditorKit editorKit = new HTMLEditorKit();
+		applicationContentPane.setEditorKit(editorKit);
+		Document document = editorKit.createDefaultDocument();
+		applicationContentPane.setDocument(document);
+		tabbedPane.add("Application", new JScrollPane(applicationContentPane));
+
+		// System info content
+		DefaultTableModel tableModel = new DefaultTableModel() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		tableModel.addColumn("Property");
+		tableModel.addColumn("Value");
+		Properties props = System.getProperties();
+		List<String> propertyNames = new ArrayList<>(props.stringPropertyNames());
+		Collections.sort(propertyNames);
+		for (String propName: propertyNames) {
+			System.out.println(propName);
+			tableModel.addRow(new Object[] { propName, System.getProperty(propName) });
+		}
+		JTable systemPropsPane = new JTable(tableModel);
+		TableColumn propNameCol = systemPropsPane.getColumnModel().getColumn(0);
+		propNameCol.setMaxWidth(300);
+		propNameCol.setPreferredWidth(200);
+		tabbedPane.add("System", new JScrollPane(systemPropsPane));
+		
 		add(tabbedPane, BorderLayout.CENTER);
 		
+		// Button Bar
 		JButton btnClose = new JButton("Close");
 		btnClose.addActionListener(e -> { JAboutDialog.this.dispose(); });
 		
--- src/test/java/com/github/sixro/jaboutdialog/Launcher.java
+++ src/test/java/com/github/sixro/jaboutdialog/Launcher.java
@@ -1,18 +1,61 @@
 package com.github.sixro.jaboutdialog;
 
 import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.SwingUtilities;
+
+import org.apache.commons.io.IOUtils;
 
 public class Launcher {
 
 	public static void main(String... args) {
-		//System.setProperty("swing.default.hgap", "12");
-		
-		JAboutDialog dialog = new JAboutDialog((Window) null, "About...");
-		dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		dialog.setSize(400, 300);
-		dialog.setVisible(true);
+		//System.setProperty("swing.default.hgap", "12");		 
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				JFrame.setDefaultLookAndFeelDecorated(true);
+				JDialog.setDefaultLookAndFeelDecorated(true);
+				
+				JFrame frame = new JFrame("My App");
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				JMenuBar menubar = new JMenuBar();
+				JMenu mnuHelp = new JMenu("Help");
+				mnuHelp.add(new JMenuItem(new AbstractAction("About") {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						JAboutDialog dialog = new JAboutDialog(frame, "About...");
+						dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+						String html = readContent("/app-content.html");
+						dialog.setApplicationContent(html);
+						dialog.setSize(400, 300);
+						dialog.setVisible(true);
+					}
+				}));
+				menubar.add(mnuHelp);
+				frame.setJMenuBar(menubar);
+				frame.setSize(800, 600);
+				frame.setVisible(true);				
+			}
+		});
 	}
-	
+
+	public static String readContent(String resourcePath) {
+		try (InputStream in = Launcher.class.getResourceAsStream(resourcePath)) {
+			return IOUtils.toString(in, "UTF-8");			
+		} catch (IOException e) {
+			throw new RuntimeException("Unable to read content of '" + resourcePath + "'", e);
+		}
+	}
+
 }
